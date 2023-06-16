dd18a05cfc225bd87fb03c61141665362cdaa071
Leul <leulassefa255@gmail.com>
2023-06-16 05:43:49 -0700

    Variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,17 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
+	char letter;
 
-    char letter;
+	for (letter = 'a'; letter <= 'z'; letter++) {
+        	putchar(letter);
+        }
 
-    for (letter = 'a'; letter <= 'z'; letter++) {
-        putchar(letter);
-    }
+        putchar('\n');
 
-    putchar('\n');
-
-    return 0;
-
+        return 0;
 }
