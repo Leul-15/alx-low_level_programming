0536efb53ada511f333e09fa887b6dc9c83751a1
Leul <leulassefa255@gmail.com>
2023-06-16 05:58:41 -0700

    Variables if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,29 @@
  */
 int main(void)
 {
-int nums;
-int nums1;
+int i, j;
 
-for (nums = '0'; nums <= '9'; nums++)
-{
-for (nums1 = '0'; nums1 <= '9'; nums1++)
-{
-putchar(nums);
-putchar(nums1);
-if (nums == '9' && nums1 == '9')
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,8 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main() 
-{
-	
+{	
     	char letter;
 
    	 for (letter = 'a'; letter <= 'z'; letter++) {
