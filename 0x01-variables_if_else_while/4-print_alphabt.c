6d86a73a7029f8794c6dd4d05b8961570b0327ca
Leul <leulassefa255@gmail.com>
2023-06-16 06:15:40 -0700

    Variables if else while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
-{
+int main() {
     char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-       	{
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        if (letter != 'q' && letter != 'e') {
             putchar(letter);
         }
     }
