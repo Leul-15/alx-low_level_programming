77bbcd2729db56dc8777d2a05f16a377e5cbbeeb
Leul <leulassefa255@gmail.com>
2023-06-29 04:13:53 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char data2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; data1[j] != '\0'; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = data2[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
