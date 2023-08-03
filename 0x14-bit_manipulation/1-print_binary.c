ce1c8fb63813117c7d20d3730cbb6e4f8cc30c24
Leul <leulassefa255@gmail.com>
2023-08-03 11:12:43 -0700

    C - Bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int num, c = 0;
+
+	unsigned long int ct;
+
+	for (num = 63; num >= 0; num--)
+	{
+		ct = n >> num;
+
+		if (ct & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+			_putchar('0');
+	}
+	if (!c)
+		_putchar('0');
+}
